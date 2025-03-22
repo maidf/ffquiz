d665aa1b9e827475639f938262733331aa6d8a5a
maidf <ml@fufu.moe>
2025-03-22 10:58:23 +0000

    创建service层的基本接口

---
+++ ffquiz/common/src/main/java/com/ff/common/service/QuestionBankService.java
@@ -0,0 +1,8 @@
+package com.ff.common.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ff.common.entity.po.QuestionBank;
+
+public interface QuestionBankService extends IService<QuestionBank> {
+
+}
---
+++ ffquiz/common/src/main/java/com/ff/common/service/QuestionService.java
@@ -0,0 +1,8 @@
+package com.ff.common.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ff.common.entity.po.Question;
+
+public interface QuestionService extends IService<Question> {
+
+}
