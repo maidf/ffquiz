152f5d0fb6cd4f21bb9c49046ce32d7ddec7a48e
maidf <ml@fufu.moe>
2025-04-15 00:49:07 +0000

    update

--- devenv.nix
+++ devenv.nix
@@ -8,12 +8,7 @@ let
 in
 {
   cachix.enable = false;
-
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
-  # packages = [ pkgs.git ];
+  devcontainer.enable = true;
 
   languages.java = {
     enable = true;
@@ -44,7 +39,7 @@ in
     ];
     initialDatabases = [
       {
-        name = "quiz";
+        name = "quiz_db";
         schema = ./db/quiz.sql;
       }
     ];
@@ -52,34 +47,7 @@ in
 
   services.redis.enable = true;
 
-  # 项目启动流程
-  # processes = {
-  #   backend = {
-  #     exec = "cd javaquiz && mvn spring-boot:run";
-  #     process-compose.depends_on = {
-  #       redis.condition = "process_running";
-  #       mysql.condition = "process_running";
-  #     };
-  #   };
-  #   frontend.exec = "cd uniquiz && pnpm dev:h5";
-  # };
-
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
   enterShell = ''
-    hello
     java --version
     node -v
     pnpm -v
